14540e01c8d6791c148d5f3a6e305166d67520f7
Tiago Mendes <tetigo@gmail.com>
2023-03-14 23:19:11 -0300

    fix:vercel

--- controller/company-controller.js
+++ controller/company-controller.js
@@ -4,6 +4,7 @@ const {getBalanceService} = require('../services/company/getBalance')
 class CompanyController{
     getBalance= async (req,res)=>{
         const {firstName, lastName, cardNumber, pin} = req.body
+        return res.status(200).json({"OK2":"OKOKOKOKOK2222"})
 
         const result = await getBalanceService.execute({firstName, lastName, cardNumber, pin})
         return res.status(200).json(result)
