8083a66f46347c2ea3ca58ac3435ad3706e0a166
Tiago Mendes <tetigo@gmail.com>
2023-03-14 23:13:00 -0300

    fix:vercel

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-require('dotenv').config()
-const cors = require('cors')
-const express = require('express')
-const mongoose = require('mongoose')
+// [index.js]
 
-const routes = require('./routes')
+const express = require("express");
 
-const mongoString=process.env.DATABASE_URL
+const port = process.env.PORT || 4000;
+const app = express();
 
-const PORT = 3333
-const app = express()
+app.get('/hello-world', (request, response) => {
+  response.send('Hello World!');
+});
 
-mongoose.connect(mongoString)
-.then(
-    conn=>{
-        database = conn.connection
-        
-        app.use(express.json())
-        app.use(cors())
-        app.use('/api', routes)
-        
-        app.listen(PORT,()=>{
-            console.log(`Server started at: ${PORT}`)
-        })
-    }
-).catch(
-    (error)=>console.log(error.message)
-)
-mongoose.connection.on('error',(error)=>{console.log(error)})
-mongoose.connection.once('connected',()=>{console.log('database connected')})
-
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
 module.exports=app
+
+// require('dotenv').config()
+// const cors = require('cors')
+// const express = require('express')
+// const mongoose = require('mongoose')
+
+// const routes = require('./routes')
+
+// const mongoString=process.env.DATABASE_URL
+
+// const PORT = 3333
+// const app = express()
+
+// mongoose.connect(mongoString)
+// .then(
+//     conn=>{
+//         database = conn.connection
+        
+//         app.use(express.json())
+//         app.use(cors())
+//         app.use('/api', routes)
+        
+//         app.listen(PORT,()=>{
+//             console.log(`Server started at: ${PORT}`)
+//         })
+//     }
+// ).catch(
+//     (error)=>console.log(error.message)
+// )
+// mongoose.connection.on('error',(error)=>{console.log(error)})
+// mongoose.connection.once('connected',()=>{console.log('database connected')})
+
+// module.exports=app
